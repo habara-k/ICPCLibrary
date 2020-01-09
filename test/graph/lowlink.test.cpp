ab58a42b76d89a0ce09485866ce49902761d3ec1
Keigo Habara <k.habara.aa@gmail.com>
2020-01-09 16:39:20 +0900

    Fix test file (lowlink)

--- test/graph/lowlink.test.cpp
+++
@@ -1,3 +0,0 @@
-// bridge: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_B
-// articulation: http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_A
-
---
+++ test/graph/lowlink/bridge.test.cpp
@@ -0,0 +1,1 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/3/GRL_3_B"
