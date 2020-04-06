a54fbf94a8843877cbc47cbdee5c3f38dbee4f66
Yohei Maeda <mdyh2a38024@gmail.com>
2020-04-07 03:51:03 +0900

    Add test for bfs01.test.cpp

---
+++ test/graph/bfs01.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_A"
+
+#include "../../graph/dijkstra.cpp"
+
+int main() {
+    int V, E, R;
+    cin >> V >> E >> R;
+    Graph<int> g(V);
+    for (int i = 0; i < E; ++i) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        g[a].push_back({a,b,c});
+    }
+    for (auto &dist : dijkstra(g, R)) {
+        if (dist == numeric_limits<int>::max()) puts("INF");
+        else cout << dist << endl;
+    }
+}
