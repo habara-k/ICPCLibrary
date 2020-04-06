b9526748f575a04a4a5a5d4ec1cdd7007f3b5d4a
Yohei Maeda <mdyh2a38024@gmail.com>
2020-04-07 05:47:46 +0900

    Fix dinic.cpp

--- graph/dinic.cpp
+++ graph/dinic.cpp
@@ -10,86 +10,93 @@
  * @date 2019/12
  * @detail
  * 2020/04/07 コメント追加 by Md
+ * 構築したい
  */
 
-template<typename T>
+template <typename T>
 struct Dinic {
-    const T INF;
+  int sz;
+  T inf = numeric_limits<T>::max();
+  vector<int> level, iter;
 
-    struct edge {
-        int to;
-        T cap;
-        int rev;
-        bool isrev;
-    };
+  struct Edge {
+    int to, rev;
+    T cap;
+    Edge(int to, int rev, T cap): to(to), rev(rev), cap(cap) {}
+  };
+  vector<vector<Edge>> g;
 
-    vector<vector<edge>> g;
-    vector<int> level, iter;
+  /**
+   * @brief コンストラクタ
+   * @param V 頂点数
+   */
+  Dinic(int V): sz(V) {
+    g.resize(V);
+    level.resize(V);
+    iter.resize(V);
+  };
 
-    /**
-     * @param V ネットワークの頂点数
-     */
-    Dinic(int V) : INF(numeric_limits<T>::max()), g(V) {}
+  /**
+   * @brief 辺を追加
+   * @param[in] from 始点
+   * @param[in] to 終点
+   * @param[in] cap 容量
+   */
+  void add_edge(int from, int to, T cap) {
+    g[from].emplace_back(to, (int)(g[to].size()), cap);
+    g[to].emplace_back(from, (int)(g[from].size())-1, 0);
+  }
 
-    /**
-     * @brief ネットワークに辺を追加
-     * @param from 始点
-     * @param to 終点
-     * @param cap 容量
-     */
-    void add_edge(int from, int to, T cap) {
-        g[from].push_back({to, cap, (int)g[to].size(), false});
-        g[to].push_back({from, 0, (int)g[from].size()-1, true});
+  /**
+   * @brief 最大流を求める
+   * @param[in] s 始点
+   * @param[in] t 終点
+   */
+  T max_flow(int s, int t) {
+    T flow = 0;
+    while(1) {
+      bfs(s);
+      if(level[t] < 0) return flow;
+      iter.assign(sz, 0);
+      T f = dfs(s, t, inf);
+      while(f > 0) {
+        flow += f;
+        f = dfs(s, t, inf);
+      }
     }
-
-    /**
-     * @brief 始点と終点を決めて最大流を求める
-     * @param s 始点
-     * @param t 終点
-     * @return T 流量
-     */
-    T max_flow(int s, int t) {
-        T flow = 0;
-        while (bfs(s, t)) {
-            iter.assign((int)(g.size()), 0);
-            T f = 0;
-            while((f = dfs(s, t, INF)) > 0) flow += f;
-        }
-        return flow;
-    }
+  }
 
 private:
-    bool bfs(int s, int t) {
-        level.assign((int)(g.size()), -1);
-        queue<int> que;
-        level[s] = 0;
-        que.push(s);
-        while (!que.empty()) {
-            int v = que.front();
-            que.pop();
-            for (auto &e : g[v]) {
-                if (e.cap > 0 && level[e.to] == -1) {
-                    level[e.to] = level[v] + 1;
-                    que.push(e.to);
-                }
-            }
+  void bfs(int s) {
+    level.assign(sz, -1);
+    level[s] = 0;
+    queue<int> que;
+    que.push(s);
+    while(!que.empty()) {
+      int now = que.front(); que.pop();
+      for(auto &e: g[now]) {
+        if(e.cap > 0 && level[e.to] < 0) {
+          level[e.to] = level[now] + 1;
+          que.push(e.to);
         }
-        return level[t] != -1;
+      }
     }
+  }
 
-    T dfs(int v, const int t, T flow) {
-        if (v == t) return flow;
-        for (int &i = iter[v]; i < (int)(g[v].size()); i++) {
-            edge &e = g[v][i];
-            if (e.cap > 0 && level[v] < level[e.to]) {
-                T d = dfs(e.to, t, min(flow, e.cap));
-                if (d > 0) {
-                    e.cap -= d;
-                    g[e.to][e.rev].cap += d;
-                    return d;
-                }
-            }
+  T dfs(int s, int t, T flow) {
+    if(s == t) return flow;
+    for(int i=iter[s];i<(int)(g[s].size());++i) {
+      iter[s] = i;
+      auto e = g[s][i];
+      if(e.cap > 0 && level[s] < level[e.to]) {
+        T d = dfs(e.to, t, min(flow, e.cap));
+        if(d > 0) {
+          g[s][i].cap -= d;
+          g[e.to][e.rev].cap += d;
+          return d;
         }
-        return 0;
+      }
     }
+    return 0;
+  }
 };
