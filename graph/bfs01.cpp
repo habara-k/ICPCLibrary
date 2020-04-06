ce7ae5479da948face15f1f3564985ade5246072
Yohei Maeda <mdyh2a38024@gmail.com>
2020-04-07 03:50:49 +0900

    Fix bfs01

--- graph/bfs01.cpp
+++ graph/bfs01.cpp
@@ -1,9 +1,25 @@
-#include "../template.cpp"
+#include "template.cpp"
 
 // verify: https://codeforces.com/contest/590/problem/C
 
+/**
+ * @brief
+ * 01-BFS
+ * 辺の重みが01の時にO(E+V)で単一始点最短路をやる
+ * @author Md
+ * @date 2019/12
+ * @param[in] g グラフ
+ * @param[in] s 始点
+ * @return vector<T> sからそれぞれの頂点への最短路
+ * 
+ * @details
+ * 2020/04/07
+ * ソースコード修正、コメント追加、テスト追加 by Md
+ * テストはdijkstraのものを流用(01-BFSでしか通らない問題、あるか？)
+ */
+
 template <typename T>
-std::vector<T> bfs01(const vector<vector<edge<T>>> &g, int s) {
+std::vector<T> bfs01(const Graph<T> &g, int s) {
     const T INF = numeric_limits<T>::max();
     std::vector<T> res(SZ(g), INF);
     deque<pair<T, int>> deq;
