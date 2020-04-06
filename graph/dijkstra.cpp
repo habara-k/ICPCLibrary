0dcbf08395320dc2e352ea0d3377b6b589d37d13
Yohei Maeda <mdyh2a38024@gmail.com>
2020-04-07 03:36:56 +0900

    Add comment on dijkstra

--- graph/dijkstra.cpp
+++ graph/dijkstra.cpp
@@ -1,5 +1,20 @@
 #include "template.cpp"
 
+/**
+ * @brief
+ * 単一始点最短路(ダイクストラ)
+ * 二分ヒープ(priority_queue)を使ってO((E+V)logV)
+ * @author ?
+ * @date 2019/12
+ * 
+ * @param[in] g グラフ
+ * @param[in] s 始点
+ * @return vector<T> sからそれぞれの頂点への最短路
+ * 
+ * @details
+ * 2020/04/07 コメント追加、テスト有無のチェック by Md
+ */
+
 template<typename T>
 vector<T> dijkstra(const Graph<T> &g, int s) {
     const auto INF = numeric_limits<T>::max();
