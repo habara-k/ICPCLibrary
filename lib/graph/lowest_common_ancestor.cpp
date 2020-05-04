54a37e83efe019fa6e6397ba6c5a8574d78b4a48
Keigo Habara <k.habara.aa@gmail.com>
2020-05-04 16:42:34 +0900

    Refactor LCA

--- lib/graph/lowest_common_ancestor.cpp
+++ lib/graph/lowest_common_ancestor.cpp
@@ -13,6 +13,52 @@
  */
 
 struct LCA {
+
+    /**
+    * @brief コンストラクタ. O(nlog n)
+    * @param[in] G 無向木.
+    * @param[in] root 指定した根.
+    */
+    LCA(const vector<vector<int>>& G, int root = 0) :
+            n(G.size()), log2_n(log2(n)), depth(n),
+            par(log2_n+1, vector<int>(n,-1)) {
+
+        dfs(G, root, -1, 0);
+
+        for (int k = 0; k < log2_n; ++k) {
+            for (int v = 0; v < n; ++v) {
+                if (par[k][v] != -1) {
+                    par[k+1][v] = par[k][par[k][v]];
+                }
+            }
+        }
+    }
+
+    /**
+    * @brief lca を取得する. O(log n);
+    * @param[in] u, v: lca を求めたい2頂点.
+    * @return u, v のlca.
+    */
+    int query(int u, int v) {
+        if (depth[u] > depth[v]) swap(u, v);
+
+        for (int k = 0; k <= log2_n; ++k) {
+            if ((depth[v] - depth[u]) >> k & 1) {
+                v = par[k][v];
+            }
+        }
+        if (u == v) return u;
+
+        for (int k = log2_n; k >= 0; --k) {
+            if (par[k][u] != par[k][v]) {
+                u = par[k][u];
+                v = par[k][v];
+            }
+        }
+        return par[0][u];
+    }
+
+private:
     int n, log2_n;
     vector<int> depth;
     vector<vector<int>> par;
@@ -23,41 +69,5 @@ struct LCA {
         for (auto to : G[v]) {
             if (to != p) dfs(G, to, v, d+1);
         }
-    }
-
-    LCA(const vector<vector<int>>& G, int root=0) :
-        n(G.size()), log2_n(log2(n)), depth(n),
-        par(log2_n+1, vector<int>(n,-1)) {
-
-            dfs(G, root, -1, 0);
-
-            for (int k = 0; k < log2_n; ++k) {
-                for (int v = 0; v < n; ++v) {
-                    if (par[k][v] != -1) {
-                        par[k+1][v] = par[k][par[k][v]];
-                    }
-                }
-            }
-        }
-
-    int query(int u, int v) {
-        if (depth[u] > depth[v]) swap(u, v);
-
-        // align the depth of u and v
-        for (int k = 0; k <= log2_n; ++k) {
-            if ((depth[v] - depth[u]) >> k & 1) {
-                v = par[k][v];
-            }
-        }
-        if (u == v) return u;
-
-        // go back until u and v's parents do not match
-        for (int k = log2_n; k >= 0; --k) {
-            if (par[k][u] != par[k][v]) {
-                u = par[k][u];
-                v = par[k][v];
-            }
-        }
-        return par[0][u];
     }
 };
