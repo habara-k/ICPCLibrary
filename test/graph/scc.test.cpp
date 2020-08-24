0ded4a83cbe30e8e474116412a9583a23f1fa897
Keigo Habara <k.habara.aa@gmail.com>
2020-08-24 14:33:03 +0900

    Refactor scc

--- test/graph/scc.test.cpp
+++ test/graph/scc.test.cpp
@@ -1,21 +1,23 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/3/GRL_3_C"
 
-#include "../../lib/graph/scc.cpp"
+#include "../../../lib/graph/scc.cpp"
 
 int main() {
     int V, E, Q;
     cin >> V >> E;
-    Graph<int> g(V);
-    for (int i = 0; i < E; i++) {
+    vector<vector<int>> G(V);
+    for (int i = 0; i < E; ++i) {
         int a, b; cin >> a >> b;
-        g[a].push_back({a, b, 0});
+        G[a].push_back(b);
     }
-    SCC<int> scc(g);
-    scc.build(g);
-    vector<int> comp = scc.get_comp();
+
+    StronglyConnectedComponents scc(G);
+    scc.build();
+
     cin >> Q;
     while (Q--) {
-        int a, b; cin >> a >> b;
-        puts(comp[a] == comp[b] ? "1" : "0");
+        int a, b;
+        cin >> a >> b;
+        cout << (scc.comp[a] == scc.comp[b] ? "1" : "0") << endl;
     }
 }
