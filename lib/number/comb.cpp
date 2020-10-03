5bfba4c1d2cd5aa6a73d4df233e93625b0464a8e
Keigo Habara <k.habara.aa@gmail.com>
2020-10-03 18:15:24 +0900

    extend bounds automatically

--- lib/number/comb.cpp
+++ lib/number/comb.cpp
@@ -4,34 +4,48 @@
 
 template<typename Field>
 struct Combination {
-    vector<Field> _fact, _rfact, _inv;
+    int _n = 1;
+    vector<Field> _fact{1}, _rfact{1}, _inv{1};
 
-    Combination(int n) : _fact(n), _rfact(n), _inv(n) {
-        _fact[0] = _rfact[n-1] = 1;
-        for (int i = 1; i < n; ++i) _fact[i] = _fact[i-1] * i;
-        _rfact[n-1] /= _fact[n-1];
-        for (int i = n-1; i > 0; --i) _rfact[i-1] = _rfact[i] * i;
-        for (int i = 1; i < n; ++i) _inv[i] = _rfact[i] * _fact[i-1];
+    void extend(int n) {
+        _fact.resize(n);
+        _rfact.resize(n);
+        _inv.resize(n);
+        for (int i = _n; i < n; ++i) _fact[i] = _fact[i - 1] * i;
+        _rfact[n - 1] = 1 / _fact[n - 1];
+        for (int i = n - 1; i > _n; --i) _rfact[i - 1] = _rfact[i] * i;
+        for (int i = _n; i < n; ++i) _inv[i] = _rfact[i] * _fact[i - 1];
+        _n = n;
     }
 
-    inline Field fact(int k) const { return _fact.at(k); }
+    Field fact(int k) {
+        if (_n <= k) extend(k + 1);
+        return _fact.at(k);
+    }
 
-    inline Field rfact(int k) const { return _rfact.at(k); }
+    Field rfact(int k) {
+        if (_n <= k) extend(k + 1);
+        return _rfact.at(k);
+    }
 
-    inline Field inv(int k) const { assert(k != 0); return _inv.at(k); }
+    Field inv(int k) {
+        assert(k != 0);
+        if (_n <= k) extend(k + 1);
+        return _inv.at(k);
+    }
 
-    Field P(int n, int r) const {
+    Field P(int n, int r) {
         if (r < 0 or n < r) return 0;
-        return fact(n) * rfact(n-r);
+        return fact(n) * rfact(n - r);
     }
 
-    Field C(int n, int r) const {
+    Field C(int n, int r) {
         if (r < 0 or n < r) return 0;
-        return fact(n) * rfact(r) * rfact(n-r);
+        return fact(n) * rfact(r) * rfact(n - r);
     }
 
-    Field H (int n, int r) const {
-        return (n == 0 and r == 0) ? 1 : C(n+r-1, r);
+    Field H(int n, int r) {
+        return (n == 0 and r == 0) ? 1 : C(n + r - 1, r);
     }
 };
 
