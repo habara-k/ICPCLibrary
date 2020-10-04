da3f8cd228af310cfd21a99ab172c626d2143bca
Yohei Maeda <mdyh2a380824@gmail.com>
2020-10-05 05:06:38 +0900

    Fix combination.cpp

--- lib/number/combination.cpp
+++ lib/number/combination.cpp
@@ -85,8 +85,8 @@ void partition_fast(ll n, ll m) {
     P2[0] = 1;
     for(int i=1;i<100;++i) {
         for(int j=1, sign = 1; i - (j*j*3-j)/2 >= 0; ++j, sign *= -1) {
-            P2[i] += P[i-(j*j*3-j)/2] * sign;
-            if(i - (j*j*3+j)/2 >= 0) P[i] += P[i-(j*j*3+j)/2] * sign;
+            P2[i] += P2[i-(j*j*3-j)/2] * sign;
+            if(i - (j*j*3+j)/2 >= 0) P2[i] += P2[i-(j*j*3+j)/2] * sign;
         }
     }
 }
