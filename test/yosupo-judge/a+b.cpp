e3a1b106b0b58276b0707a6e3ed78d3a7ec1d20e
Keigo Habara <k.habara.aa@gmail.com>
2020-10-11 12:49:59 +0900

    Add Many A + B

--- test/yosupo-judge/a+b.cpp
+++ test/yosupo-judge/a+b.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_A"
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 
 #include "../../lib/template.cpp"
 
---
+++ test/yosupo-judge/many_a+b.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/many_aplusb"
+
+#include "../../lib/template.cpp"
+
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        ll a, b; cin >> a >> b;
+        cout << a + b << endl;
+    }
+}
