f3c6c3ffad028d20ed729cbb876c9d8ddad653d3
Keigo Habara <k.habara.aa@gmail.com>
2020-10-11 12:43:51 +0900

    Add A + B

---
+++ test/yosupo-judge/a+b.cpp
@@ -0,0 +1,8 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/1/GRL_1_A"
+
+#include "../../lib/template.cpp"
+
+int main() {
+    int a, b; cin >> a >> b;
+    cout << a + b << endl;
+}
