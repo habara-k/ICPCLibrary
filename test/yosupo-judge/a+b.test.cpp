1c35a27430afbef80f235fa45da0971f40448ad5
keigo habara <34413567+habara-k@users.noreply.github.com>
2020-10-11 13:03:27 +0900

    よすぽジャッジ埋める (#33)
    * Add A + B
    
    * Add Many A + B
    
    * rename test file
    
    * [auto-verifier] verify commit 9e98a08d2bb17c13b47b9f211b2e2f53d46a6c84
    
    * fix Many A + B
    
    * [auto-verifier] verify commit 018d02537a3be00b70542cd6983c31c47c83949f
    
    Co-authored-by: GitHub <noreply@github.com>

---
+++ test/yosupo-judge/a+b.test.cpp
@@ -0,0 +1,10 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
+
+#include "../../lib/template.cpp"
+
+int main() {
+    int a, b; cin >> a >> b;
+    cout << a + b << endl;
+
+    return 0;
+}
