0527420ec674e9cd898f3f8f2b8a544b7827f93d
Keigo Habara <k.habara.aa@gmail.com>
2020-10-15 21:59:27 +0900

    Move LCA from graph to tree

--- test/tree/lowest_common_ancestor.test.cpp
+++ test/tree/lowest_common_ancestor.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_C"
 
-#include "../../lib/graph/lowest_common_ancestor.cpp"
+#include "../../lib/tree/lowest_common_ancestor.cpp"
 
 int main() {
     int N, Q;
