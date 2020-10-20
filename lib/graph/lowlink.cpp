81a090b20cba4a2920d5dfd3fc277300c789c0c3
kanra824 <mdyh2a380824@gmail.com>
2020-10-20 21:50:11 +0900

    Update lowlink

--- lib/graph/lowlink.cpp
+++ lib/graph/lowlink.cpp
@@ -1,57 +1,43 @@
 #include "./template.cpp"
 
-template<typename T>
-struct LowLink {
-    const int inf = 1000000000;
-    int sz;
-    std::vector<int> pre, low;
-    std::vector<bool> sel;
-    std::vector<std::pair<int, int>> bridge;
-    std::vector<int> articulation;
+struct Lowlink {
+  vvi g;
+  int n;
+  vi ord, low;
+  vector<P> bridge;
+  vector<int> articulation;
+  int t = 0;
 
-    LowLink(const Graph<T> &g) {
-        sz = g.size();
-        pre.resize(sz, inf);
-        low.resize(sz, inf);
-        sel.resize(sz, false);
-        int cnt = 0;
-        dfs(g, 0, -1, cnt);
-    }
+  Lowlink(const vvi &G): g(G) {
+    n = SZ(g);
+    ord.resize(n, INF);
+    low.resize(n, INF);
 
-    void dfs(const Graph<T> &g, int now, int prev, int &cnt) {
-        if(pre[now] != inf) {
-            low[prev] = min(low[prev], pre[now]);
-            return;
-        }
-        pre[now] = cnt;
-        low[now] = cnt;
-        cnt++;
-        for(int i=0;i<(int)(g[now].size());++i) {
-            int nxt = g[now][i].to;
-            //if g is an undirected graph
-            if(nxt == prev) continue;
-            dfs(g, nxt, now, cnt);
-        }
-        if(prev != -1) low[prev] = min(low[prev], low[now]);
-        if(prev != -1 && pre[prev] < low[now]) {
-            bridge.emplace_back(make_pair(prev, now));
-        }
-    }
+    dfs(0, -1);
+  }
 
-    void get_articulation(const Graph<T> &g, int now, int prev) {
-        sel[now] = true;
-        int art = 0;
-        for(int i=0;i<(int)(g[now].size());++i) {
-            int nxt = g[now][i].to;
-            //cout << now << ":" << nxt << endl;
-            if(sel[nxt]) continue;
-            // if g is an undirected graph
-            if(nxt == prev) continue;
-            if(now == 0 || pre[now] <= low[nxt]) art++;
-            get_articulation(g, nxt, now);
+  void dfs(int now, int prev) {
+    ord[now] = t;
+    low[now] = ord[now];
+    t++;
+    int d = 0;
+    bool ar = false;
+    for(auto &nxt: g[now]) {
+      if(nxt == prev) continue;
+      if(ord[nxt] == INF) {
+        d++;
+        dfs(nxt, now);
+        chmin(low[now], low[nxt]);
+        ar |= prev != -1 && ord[now] <= low[nxt];
+        if(ord[now] < low[nxt]) {
+          if(now < nxt) bridge.emplace_back(now, nxt);
+          else bridge.emplace_back(nxt, now);
         }
-        if((now == 0 && art >= 2) || (now != 0 && art >= 1)) {
-            articulation.push_back(now);
-        }
+      } else {
+        chmin(low[now], low[nxt]);
+      }
     }
+    ar |= prev == -1 && d >= 2;
+    if(ar) articulation.push_back(now);
+  }
 };
