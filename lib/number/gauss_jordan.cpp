40cc76e35f78047c0518c998af059af9158a59f4
Keigo Habara <k.habara.aa@gmail.com>
2020-10-26 01:15:17 +0900

    Fix Matrix

--- lib/number/gauss_jordan.cpp
+++ lib/number/gauss_jordan.cpp
@@ -1,38 +1,63 @@
 #include "../template.cpp"
 
-/**
- * @brief
- * ガウスの消去法
- * 行列をランクを求めたり、Ax = b を解いたりする
- * @author habara-k
- * @date 2020/4
- * @param[in] mat 行列
- * @return int mat のランク
+/*
+ * @brief ガウスの消去法, 連立方程式
+ * @ref https://drken1215.hatenablog.com/entry/2019/03/20/000200
+ * @date 2020/10/26
+ * @details
  */
-
 template<class T>
-int GaussJordanElimination(vector<vector<T>>& mat) {
-    int H = mat.size(), W = mat[0].size(), rank = 0;
-    for (int col = 0; col < W; ++col) {
+using Matrix = vector<vector<T>>;
+
+template<class T> int GaussJordan(Matrix<T> &A, bool is_extended = false) {
+    int m = A.size(), n = A[0].size();
+    int rank = 0;
+    for (int col = 0; col < n; ++col) {
+        if (is_extended && col == n-1) break;
         int pivot = -1;
-        for (int row = rank; row < H; ++row) {
-            if (mat[row][col] != 0) {
+        // Tがmodintのときは, A[row][pivot] != 0 なるpivotを選べばよい
+        T ma = eps;
+        for (int row = rank; row < m; ++row) {
+            if (abs(A[row][col]) > ma) {
+                ma = abs(A[row][col]);
                 pivot = row;
-                break;
             }
         }
         if (pivot == -1) continue;
-        swap(mat[rank], mat[pivot]);
-        T topLeft = mat[rank][col];
-        for (int c = col; c < W; ++c) {
-            mat[rank][c] /= topLeft;
-        }
-        for (int row = rank+1; row < H; ++row) {
-            T ratio = mat[row][col];
-            for (int c = col; c < W; ++c)
-                mat[row][c] -= ratio * mat[rank][c];
+        swap(A[pivot], A[rank]);
+        auto fac = A[rank][col];
+        for (int col2 = 0; col2 < n; ++col2) A[rank][col2] /= fac;
+        for (int row = 0; row < m; ++row) {
+            if (row != rank && abs(A[row][col]) > eps) {
+                auto fac = A[row][col];
+                for (int col2 = 0; col2 < n; ++col2) {
+                    A[row][col2] -= A[rank][col2] * fac;
+                }
+            }
         }
         ++rank;
     }
-    return (rank);
+    return rank;
 }
+
+
+template<class T> vector<T> linear_equation(Matrix<T> A, vector<T> b) {
+    // extended
+    int m = A.size(), n = A[0].size();
+    Matrix<T> M(m, vector<T>(n + 1));
+    for (int i = 0; i < m; ++i) {
+        for (int j = 0; j < n; ++j) M[i][j] = A[i][j];
+        M[i][n] = b[i];
+    }
+    int rank = GaussJordan(M, true);
+
+    // check if it has no solution
+    vector<T> res;
+    for (int row = rank; row < m; ++row) if (abs(M[row][n]) > eps) return res;
+
+    // answer
+    res.assign(n, 0);
+    for (int i = 0; i < rank; ++i) res[i] = M[i][n];
+    return res;
+}
+
