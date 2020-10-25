b319eabf30e475b2abcb1f21331dc6eb9b1f2646
Keigo Habara <k.habara.aa@gmail.com>
2020-10-26 01:20:14 +0900

    Fix test url

--- test/number/gauss_jordan.test.cpp
+++ test/number/gauss_jordan.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=1308"
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=2171"
 #define ERROR "1e-8"
 
 #include "../../lib/number/gauss_jordan.cpp"
