eed37de79699112906d0520deda4a9e169f5587c
Keigo Habara <k.habara.aa@gmail.com>
2020-10-26 21:37:53 +0900

    Add LCA test

--- lib/graph/lowest_common_ancestor.cpp
+++ lib/graph/lowest_common_ancestor.cpp
@@ -1,30 +1,23 @@
 #include "../template.cpp"
 
 /**
- * @brief
- * 最小共通祖先（ダブリング）
- * 構築O(VlogV), クエリO(logV)
+ * @brief LCA
+ * @author habara-k
+ * @date 2020/10/26
+ * @usage
+ * vector<vector<int>> g(n);
+ * int root;
+ * LCA lca(g, root); // rootを根としてLCAを構築. O(nlog n)
  *
- * LCA(G, root)で構築（無向木Gを、rootを根として向きづけしたときのLCAを構築）
- * query(u, v)で取得
- *
- * @author ゆきのん？
- * @date 2019/12
+ * int u, v;
+ * lca.get(u, v); // u,vのLCAを取得. O(log n)
  */
-
 struct LCA {
 
-    /**
-    * @brief コンストラクタ. O(nlog n)
-    * @param[in] G 無向木.
-    * @param[in] root 指定した根.
-    */
-    LCA(const vector<vector<int>>& G, int root = 0) :
+    LCA(const vector<vector<int>>& G, int root) :
             n(G.size()), log2_n(log2(n)), depth(n),
             par(log2_n+1, vector<int>(n,-1)) {
-
         dfs(G, root, -1, 0);
-
         for (int k = 0; k < log2_n; ++k) {
             for (int v = 0; v < n; ++v) {
                 if (par[k][v] != -1) {
@@ -34,21 +27,14 @@ struct LCA {
         }
     }
 
-    /**
-    * @brief lca を取得する. O(log n);
-    * @param[in] u, v: lca を求めたい2頂点.
-    * @return u, v のlca.
-    */
     int query(int u, int v) {
         if (depth[u] > depth[v]) swap(u, v);
-
         for (int k = 0; k <= log2_n; ++k) {
             if ((depth[v] - depth[u]) >> k & 1) {
                 v = par[k][v];
             }
         }
         if (u == v) return u;
-
         for (int k = log2_n; k >= 0; --k) {
             if (par[k][u] != par[k][v]) {
                 u = par[k][u];
---
+++ test/graph/lca.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/5/GRL/5/GRL_5_C"
+
+#include "../../lib/graph/lowest_common_ancestor.cpp"
+
+int main() {
+    int N, Q;
+    cin >> N;
+    vector<vector<int>> g(N);
+    for (int i = 0; i < N; ++i) {
+        int k; cin >> k;
+        while (k--) {
+            int c; cin >> c;
+            g[i].push_back(c);
+        }
+    }
+
+    LCA lca(g);
+
+    cin >> Q;
+    while (Q--) {
+        int x, y; cin >> x >> y;
+        printf("%d\n", lca.get(x, y));
+    }
+}
