0f4e28e5af8cfcc3b3d4a1b0d8bcff94009d997e
keigo habara <34413567+habara-k@users.noreply.github.com>
2020-10-28 03:11:36 +0900

    Refactor hash (#76)
    * Refactor hash
    
    * [auto-verifier] verify commit 80ba0ba092f47753433b8b6eb01ffb81193bda61
    
    Co-authored-by: GitHub <noreply@github.com>

--- lib/string/rolling_hash.cpp
+++ lib/string/rolling_hash.cpp
@@ -1,76 +1,51 @@
 #include "../template.cpp"
 
 /**
-* @brief 文字列のハッシュ化
-* @author habara-k
-* @date 2020/04/26
-*/
-
+ * @brief 文字列のハッシュ化
+ * @author habara-k
+ * @date 2020/10/28
+ * @usage
+ *   auto base = RollingHash::gen_base();
+ *   string s; cin >> s;
+ *   RollingHash hash(s, base);   // O(|s|)
+ *   hash.get(l, r);   // s[l,r)のハッシュ. O(1)
+ */
 struct RollingHash {
-    using uint = uint64_t;
+    using u64 = uint64_t;
+    using i128 = __int128_t;
 
-    /**
-    * @brief コンストラクタ. O(|s|)
-    * @param[in] s ハッシュ化する文字列(or vector).
-    * @param[in] base ハッシュ化に使う基数. RollingHash::gen_base で作る.
-    * @details 使い方
-    *   auto base = RollingHash::gen_base();
-    *
-    *   string t; cin >> t;
-    *   RollingHash hash(t, base);
-    */
     template<typename S>
-    RollingHash(const S& s, uint base) {
+    RollingHash(const S &s, u64 base) {
         int n = s.size();
         hash.assign(n+1, 0);
         pow.assign(n+1, 1);
         for (int i = 0; i < n; ++i) {
-            hash[i+1] = mod(mul(hash[i], base) + s[i]);
+            hash[i+1] = mul(hash[i], base) + s[i];
+            if (hash[i+1] >= MOD) hash[i+1] -= MOD;
             pow[i+1] = mul(pow[i], base);
         }
     }
 
-    /**
-    * @brief ハッシュを計算する. O(1)
-    * @param[in] l, r ハッシュを計算したい区間.
-    * @return 区間[l, r) のハッシュ.
-    * @details 使い方
-    *   cout << hash.get(0, t.size()) << endl;
-    */
-    uint get(int l, int r) const {
-        return mod(hash[r] + MASK61 - mul(hash[l], pow[r - l]));
+    u64 get(int l, int r) const {
+        u64 ret = hash[r] + MOD - mul(hash[l], pow[r-l]);
+        return ret >= MOD ? ret - MOD : ret;
     }
 
-    /**
-    * @brief 基数を生成する. O(1)
-    * @return ランダムな基数.
-    * @details 使い方
-    *   auto base = RollingHash::gen_base();
-    */
-    static uint gen_base() {
+    static u64 gen_base() {
         mt19937 random{random_device{}()};
-        uniform_int_distribution<uint> dist(2, MASK61-2);
+        uniform_int_distribution<u64> dist(2, MOD-2);
         return dist(random);
     }
 
 private:
-    vector<uint> hash, pow;
-    static const uint MASK30 = (1LL<<30)-1,
-                      MASK31 = (1LL<<31)-1,
-                      MASK61 = (1LL<<61)-1;
+    vector<u64> hash, pow;
+    static const u64 MOD = (1ul << 61) - 1;
 
-    static uint mul(uint a, uint b) {
-        uint au = a >> 31, ad = a & MASK31,
-             bu = b >> 31, bd = b & MASK31;
-        uint m = au * bd + ad * bu;
-        uint mu = m >> 30, md = m & MASK30;
-
-        return mod(au*bu*2 + mu + (md<<31) + ad*bd);
-    }
-    static uint mod(uint x) {
-        uint xu = x >> 61, xd = x & MASK61;
-        uint ret = xu + xd;
-        if (ret >= MASK61) ret -= MASK61;
-        return ret;
+    static u64 mul(i128 a, i128 b) {
+        i128 t = a * b;
+        t = (t >> 61) + (t & MOD);
+        if (t >= MOD) t -= MOD;
+        return t;
     }
 };
+
