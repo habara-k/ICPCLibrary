80ba0ba092f47753433b8b6eb01ffb81193bda61
Keigo Habara <k.habara.aa@gmail.com>
2020-10-28 03:06:25 +0900

    Refactor hash

--- lib/tree/tree_hash.cpp
+++ lib/tree/tree_hash.cpp
@@ -4,32 +4,25 @@
 /**
  * @brief 木のハッシュ化
  * @author habara-k
- * @date 2020/10/14
- * @details 使い方
+ * @date 2020/10/28
+ * @usage
  *   vector<vector<int>> g;
  *   int seed = 0;
  *   auto hash = RootedTreeHash(g, seed).get();
  */
 struct TreeHash {
-    using uint = uint64_t;
+    using u64 = uint64_t;
+    using i128 = __int128_t;
 
-    /**
-     * @brief コンストラクタ. O(|n|)
-     * @param[in] g ハッシュ化する木
-     * @param[in] seed ハッシュ化に使うシード
-     */
     TreeHash(const vector<vector<int>>& g, int seed=0) : g(g) {
         int n = g.size();
         mt19937 random(seed);
-        uniform_int_distribution<uint> dist(2, MASK61 - 2);
+        uniform_int_distribution<u64> dist(2, MOD-2);
         for (int i = 0; i < n; ++i) base.emplace_back(dist(random));
     }
 
-    /**
-     * @brief ハッシュの計算. O(|n|)
-     */
-    uint get() {
-        vector<uint> hash;
+    u64 get() {
+        vector<u64> hash;
         for (int root : centroid(g)) {
             hash.emplace_back(dfs(root, -1, 0));
         }
@@ -37,35 +30,25 @@ struct TreeHash {
     }
 
 private:
-    vector<uint> base;
+    vector<u64> base;
     vector<vector<int>> g;
-    static const uint MASK30 = (1LL << 30) - 1,
-            MASK31 = (1LL << 31) - 1,
-            MASK61 = (1LL << 61) - 1;
+    static const u64 MOD = (1ul << 61) - 1;
 
-    static uint mul(uint a, uint b) {
-        uint au = a >> 31, ad = a & MASK31,
-                bu = b >> 31, bd = b & MASK31;
-        uint m = au * bd + ad * bu;
-        uint mu = m >> 30, md = m & MASK30;
-
-        return mod(au * bu * 2 + mu + (md << 31) + ad * bd);
+    static u64 mul(i128 a, i128 b) {
+        i128 t = a * b;
+        t = (t >> 61) + (t & MOD);
+        if (t >= MOD) t -= MOD;
+        return t;
     }
 
-    static uint mod(uint x) {
-        uint xu = x >> 61, xd = x & MASK61;
-        uint ret = xu + xd;
-        if (ret >= MASK61) ret -= MASK61;
-        return ret;
-    }
-
-    uint dfs(int u, int p, int d) {
-        uint hash = 1;
+    u64 dfs(int u, int p, int d) {
+        u64 hash = 1;
         for (int v : g[u]) {
             if (v == p) continue;
             hash = mul(hash, dfs(v, u, d+1));
         }
-        return mod(hash + base[d]);
+        hash += base[d];
+        return hash >= MOD ? hash - MOD : hash;
     }
 };
 
